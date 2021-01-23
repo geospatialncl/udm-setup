d9bcc8aaf28ab50b40aecd49c939e77791daf6ab
Craig-Robson <craig.robson1@ncl.ac.uk>
2021-01-23 16:47:03 +0000

    added initial script

---
+++ run.py
@@ -0,0 +1,22 @@
+"""
+Setup input data for UDM
+
+- Pull data from NISMOD-DB API
+- Rasterise data for input into UDM
+
+Issues
+- NISMOD-DB API username and password method
+- Write all data to a single directory or other output method?
+"""
+
+
+def run():
+    """
+    Inputs:
+    - LAD: a local authority district code
+    - Layer(s): List of layers to generate raster(s) for
+    - raster settings: Dict of settings for generating raster
+
+    """
+
+    return
