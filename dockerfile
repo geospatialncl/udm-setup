6fa8db9f5dcf83759be49e4ff40afa4fe858e396
Craig Robson <craig.robson1@ncl.ac.uk>
2021-02-09 18:35:33 +0000

    added method to pull env variables required

--- dockerfile
+++ dockerfile
@@ -1,6 +1,7 @@
 FROM python:3.8
 RUN apt-get -y update
 RUN apt-get -y install libgdal-dev
-RUN pip install geopandas
+COPY requirements.txt /
+RUN pip install -r requirements.txt
 RUN git clone --branch test https://github.com/geospatialncl/udm-rasteriser
-RUN pip install -r udm-rasteriser/requirements.txt
+#RUN pip install -r udm-rasteriser/requirements.txt
--- run.py
+++ run.py
@@ -9,6 +9,13 @@ Issues
 - Write all data to a single directory or other output method?
 """
 
+import requests, json
+import os
+
+api_url = os.getenv('API_URL')
+username = os.getenv('USERNAME')
+password = os.getenv('PASSWORD')
+
 
 def run():
     """
@@ -18,5 +25,21 @@ def run():
     - raster settings: Dict of settings for generating raster
 
     """
+    area = 'E08000021'
 
+    request_string = '%s/data/mastermap/areas?export_format=geojson&scale=lad&area_codes=%s&classification_codes=all' %(api_url, area)
+
+    response = requests.get(request_string, auth=(username, password))
+
+    if response.status_code != 200:
+        print('ERROR! A response code (%s) indicating no data was returned has been received from the API.' % response.status_code)
+        exit(2)
+
+    if response.status_code == 200:
+        data = json.loads(response.text)
+
+        # open file to save data to
+        with open(os.path.join('data', 'mastermap_%s.geojson' %area), 'w') as data_file:
+            json.dump(data, data_file)  # write the data from API to the file
+            print('Written data for ' + area + ' to GeoJSON file.')
     return
