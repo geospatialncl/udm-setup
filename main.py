feb069b766bfdc6bb49ab41d8ad874f5a6ba3716
Craig Robson <craig.robson1@ncl.ac.uk>
2021-11-26 08:57:28 +0000

    fixed bug caused on fork merge

--- main.py
+++ main.py
@@ -219,6 +219,6 @@ generate_attractors(available_files)
 logger.info('Attractors CSV generated')
 
 # move other files # zone id and population
-#copy_file(source='/data/inputs/zone_identity_100m.asc', dest='/data/outputs/data/zone_identity_100m.asc')
-copy_file(source='/data/inputs/zone_identity.asc', dest='/data/outputs/data/zone_identity.asc')
+copy_file(source='/data/inputs/zone_identity_100m.asc', dest='/data/outputs/data/zone_identity_100m.asc')
+#copy_file(source='/data/inputs/zone_identity.asc', dest='/data/outputs/data/zone_identity.asc')
 copy_file(source='/data/inputs/population.csv', dest='/data/outputs/data/population.csv')
